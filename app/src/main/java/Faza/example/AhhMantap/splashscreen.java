6175067d67edd2c81d4fb71be5865c6cb698da29
kamil <khasani7799@gmail.com>
2021-04-12 15:39:51 +0700

    Initial commit

---
+++ app/src/main/java/Faza/example/AhhMantap/splashscreen.java
@@ -0,0 +1,16 @@
+package Faza.example.AhhMantap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.AhhMantap.R;
+
+public class splashscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+    }
+}
